f96d6311b0ee5a70243ddfd07e3e8ca45f62aa79
Beyonce Knowles <beyonce@thinkful.com>
2015-04-28 21:44:21 -0700

    Adding the script

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
-$(document).ready(){
-	
-}
+ $(document).ready(function() {
+ $("#add").on('click', function(){
+  // fires when any LIs are clicked on
+   $userinput = $("input").val();
+   //Stored input field value in variable
+   //$('.fa-trash').hide();
+   $( "ul" ).append( $( '<li>'+$userinput+'<i class="fa fa-trash"></i></li>') );
+   //Added user Input
+   $('.fa-trash').show();
+   $("input").val("");
+   
+   //Makes form empty on click
+   //Remove item when click
+   $('li .fa ').click (function(){
+   	 //Remove item when click
+   	$(this).parent().remove();
+  
+
+
+   });
+  
+   });
+});
+
